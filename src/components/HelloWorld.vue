5ed6feac12d3c314f1a57874d9773b9c6efb1088
Pratap Mridha <pratapmridha@gmail.com>
2023-03-22 19:12:34 +0530

    sign

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,137 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+
+    <p>{{ accounts }}</p>
+    <button @click="createDidMeta">Connect Metamask </button>
+
+    <button @click="createDidkeplr">Connect keplr</button>
+    <hr>
+
+    <textarea v-model="didDocument" id="" cols="200" rows="50"></textarea>
+
   </div>
 </template>
 
 <script>
+/* eslint-disable */
+import { HypersignDID } from 'hs-ssi-sdk'
+import loadWeb3 from '../mixins/getweb3'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      didDocument: null,
+
+      hypersignDID: null,
+      web3: null,
+      keplr: null,
+      accounts: new Map()
+    }
+  },
+  mixins: [loadWeb3],
+  mounted() {
+
+
+  },
+  methods: {
+    initialise() {
+      const offlineSigner = window.keplr.getOfflineSigner('jagrat')
+      this.hypersignDID = new HypersignDID({
+        offlineSigner,
+        nodeRestEndpoint: 'http://localhost:1317',
+        nodeRpcEndpoint: 'http://localhost:26657',
+        namespce: 'testnet'
+
+      })
+
+    },
+    async createDidMeta() {
+      if (this.hypersignDID === null) {
+        this.initialise()
+
+      }
+      this.web3 = await loadWeb3(1)
+      const accounts = await this.web3.eth.getAccounts();
+
+      this.accounts.set(1, accounts[0])
+      const hexChainId = this.web3.utils.toHex(1)
+      const didDoc = await this.hypersignDID.createByClientSpec({
+        methodSpecificId: accounts[0],
+        address: accounts[0],
+        chainId: hexChainId,
+        keyType: 'EcdsaSecp256k1RecoveryMethod2020'
+      })
+      const signatureAndDoc=await this.hypersignDID.signByClientSpec({
+        didDocument:didDoc,
+        clientSpec:'eth-personalSign',
+        address:accounts[0],
+        web3:this.web3,
+        chainId:1
+      })
+
+      this.didDocument=JSON.stringify(signatureAndDoc,null,2)
+
+    },
+    async createDidkeplr() {
+      if (this.hypersignDID === null) {
+        this.initialise()
+
+      }
+      const keplrObj = await window.keplr.getKey('jagrat')
+    
+      this.keplr = window.keplr
+      this.accounts.set('jagrat', keplrObj.bech32Address)
+      const publicKeyMultibase = 'z'+this.uint8ArrayToBase58(keplrObj.pubKey)
+      const didDoc = await this.hypersignDID.createByClientSpec({
+        methodSpecificId: keplrObj.bech32Address,
+        publicKey: publicKeyMultibase,
+        address: keplrObj.bech32Address,
+        chainId: 'jagrat',
+        keyType: 'EcdsaSecp256k1VerificationKey2019'
+      })
+
+      const signatureAndDoc=await this.hypersignDID.signByClientSpec({
+        didDocument:didDoc,
+        clientSpec:'cosmos-ADR036',
+        address:keplrObj.bech32Address,
+        web3:this.keplr,
+        chainId:'jagrat'
+      })
+      this.didDocument=JSON.stringify(signatureAndDoc,null,2)
+    },
+
+    uint8ArrayToBase58(uint8array) {
+      const ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
+      const BASE = BigInt(ALPHABET.length);
+      const ZERO = BigInt(0);
+
+      // Count leading zeros
+      let leadingZeros = 0;
+      while (uint8array[leadingZeros] === 0) {
+        leadingZeros++;
+      }
+
+      let number = BigInt(0);
+      for (let i = 0; i < uint8array.length; i++) {
+        number = number * BigInt(256) + BigInt(uint8array[i]);
+      }
+
+      let encoded = '';
+      while (number > ZERO) {
+        const remainder = number % BASE;
+        encoded = ALPHABET[Number(remainder)] + encoded;
+        number = (number - remainder) / BASE;
+      }
+
+      // Add leading zeros
+      for (let i = 0; i < leadingZeros; i++) {
+        encoded = ALPHABET[0] + encoded;
+      }
+
+      return encoded;
+    }
+
+
   }
 }
 </script>
@@ -44,14 +141,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  configureWebpack: {
+    plugins: [new NodePolyfillPlugin()],
+   
+    
+  }
 })
